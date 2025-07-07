9830117184127f64c7d374f83dd59e8cbd7a82c5
majda <soussiamajda@gmail.com>
2025-07-07 22:40:06 +0200

    add

--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,29 @@
-const express = require('express');
+const express = require("express");
+const database = require("./src/Database/Database.js");
+const path = require("path");
+require("dotenv").config();
 const app = express();
-let users = [
-  { id: 1, nom: 'Alice' },
-  { id: 2, nom: 'Bob' },
-];
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// GET all users
-app.get('/', (req, res) => {
-  res.send("welcome to home");
-});
 
-// GET one user
-app.get('/users', (req, res) => {
-  if (users.length==0){
-    res.status(404),send('no users found')
-    return 
-  }
-  res.status(200).send(users)
-});
+app.use(express.json());
+database.mongoose
+  .connect(process.env.DATABASE_URL, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 
-// POST new user
-app.post('/create', (req, res) => {
-  const { nom } = req.body;
-  const newUser = { id: users.length + 1, nom };
-  users.push(newUser);
-  res.status(201).json(newUser);
+app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
+require("./src/Routes/Route.js")(app);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port ", process.env.PORT);
 });
