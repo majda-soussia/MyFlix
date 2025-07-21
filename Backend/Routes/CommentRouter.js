4e1204dc40ac45ff6325b2ddf04b14054d294ef9
jguiasma <jguiasma@gmail.com>
2025-07-21 12:45:17 +0100

    CommentRouter

--- Backend/Routes/CommentRouter.js
+++ Backend/Routes/CommentRouter.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const CommentController = require("../Controllers/CommentController");
 
-router.post("/create", CommentController.createComment);
+router.post("/create/:id", CommentController.createCommentByid);
 router.get("/", CommentController.getAllComments);
+router.put("/update/:id", CommentController.UpdateCommentByid);
+router.delete("/delete/:id", CommentController.DeleteCommentByid);
 
 
 module.exports=router;
